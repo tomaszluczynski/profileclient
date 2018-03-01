1f3d540e4a711a7112e611294ab5fae04303b06a
Tomasz Luczynski <luczynski@swissquant.com>
2018-03-01 17:15:48 +0100

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
@@ -9,6 +8,10 @@ import { ProfileService } from "./profile.service";
 import { RegistrationFormComponent } from './registration-form/registration-form.component'
 import { ReactiveFormsModule } from '@angular/forms';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModules } from './material/material.module';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 
 @NgModule({
@@ -22,7 +25,9 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AppMaterialModules,
+    FlexLayoutModule
   ],
   providers: [{
     provide: ProfileService, useClass: ProfileService
